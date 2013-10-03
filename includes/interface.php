5bd52e6d898606c4e91ecd1235b72803c447f21d
John Hughes <john@castlegateit.co.uk>
2013-10-03 11:43:00 +0100

    Only hide menus for non-admin users

--- includes/interface.php
+++ includes/interface.php
@@ -9,14 +9,18 @@
  */
 function cgit_hide_menu_items() {
 
-    if(get_option('hide_posts'))        remove_menu_page('edit.php'); // Posts
-    if(get_option('hide_media'))        remove_menu_page('upload.php'); // Media
-    if(get_option('hide_links'))        remove_menu_page('link-manager.php'); // Links
-    if(get_option('hide_pages'))        remove_menu_page('edit.php?post_type=page'); // Pages
-    if(get_option('hide_comments'))     remove_menu_page('edit-comments.php'); // Comments
+    if (!current_user_can('manage_options')) {
 
-    if(get_option('hide_categories'))   remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=category'); // Posts | Categories
-    if(get_option('hide_tags'))         remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=post_tag'); // Posts | Tags
+        if(get_option('hide_posts'))        remove_menu_page('edit.php'); // Posts
+        if(get_option('hide_media'))        remove_menu_page('upload.php'); // Media
+        if(get_option('hide_links'))        remove_menu_page('link-manager.php'); // Links
+        if(get_option('hide_pages'))        remove_menu_page('edit.php?post_type=page'); // Pages
+        if(get_option('hide_comments'))     remove_menu_page('edit-comments.php'); // Comments
+
+        if(get_option('hide_categories'))   remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=category'); // Posts | Categories
+        if(get_option('hide_tags'))         remove_submenu_page('edit.php', 'edit-tags.php?taxonomy=post_tag'); // Posts | Tags
+
+    }
 
 }
 
