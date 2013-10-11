69b4b6506b69e7afffac3276860b3e6e9a08897e
John Hughes <john@castlegateit.co.uk>
2013-10-11 14:56:56 +0100

    Even better obfuscation

--- functions.php
+++ functions.php
@@ -6,10 +6,19 @@
 function cgit_obfuscate($str) {
     $output = '';
     $forbidden = array('@', '.', ':');
-    for($i = 0; $i < strlen($str); $i++) {
+    for ($i = 0; $i < strlen($str); $i++) {
         $obfuscate = in_array($str[$i], $forbidden) ? 1 : rand(0, 1);
-        if($obfuscate) {
-            $output .= '&#' . ord($str[$i]) . ';';
+        if ($obfuscate) {
+            $code = ord($str[$i]);
+            $hex = rand(0, 1);
+            if ($hex) {
+                $code = dechex($code);
+                while (strlen($code) < 4) {
+                    $code = "0$code";
+                }
+                $code = "x$code";
+            }
+            $output .= "&#$code;";
         } else {
             $output .= $str[$i];
         }
@@ -21,8 +30,8 @@ function cgit_obfuscate($str) {
  * Generate obfuscated email link
  */
 function cgit_obfuscate_link($str, $text = false) {
-    $protocol = obfuscate('mailto:');
-    $address = obfuscate($str);
+    $protocol = cgit_obfuscate('mailto:');
+    $address = cgit_obfuscate($str);
     $output = '<a href="' . $protocol . $address . '">';
     if($text) {
         $output .= $text;
